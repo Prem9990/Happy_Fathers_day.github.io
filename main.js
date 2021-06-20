32b66dbcce54cb8ef44e815d9d8ba57a70847cd3
Prem9990 <81563032+Prem9990@users.noreply.github.com>
2021-06-20 11:53:03 +0530

    Happy Father's Day Papa

---
+++ main.js
@@ -0,0 +1,8 @@
+function A() {
+window.alert("Happy Father’s Day!");
+document.getElementById("a").play();
+}
+function B() {
+window.alert("Happy Fsther's Day!");
+document.getElementById("b").play();
+}
